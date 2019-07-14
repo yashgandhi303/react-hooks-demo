fb3db43d9c1d04ef5b415913018ae35d54833252
nick bouldien <nick.bouldien@gmail.com>
2019-07-13 20:12:34 -0500

    add firebase config ts migration

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,8 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
-import { isDev } from './constants/config';
-import { config } from './constants/firebaseConfig';
+import {isDev} from './constants/config';
+import {config} from './constants/firebaseConfig';
 
 if (isDev) {
   console.info('db config: ', config);
