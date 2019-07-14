460f40818b492f1bb241b7cf0f65fa5c7d27167b
nick bouldien <nick.bouldien@gmail.com>
2019-07-13 20:32:57 -0500

    commint ts version of LoadingSpinner

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Dimmer, Loader, Segment} from 'semantic-ui-react';
+
+const LoadingSpinner = () => (
+  <Segment>
+    <Dimmer active inverted>
+      <Loader size="medium">Loading...</Loader>
+    </Dimmer>
+  </Segment>
+);
+
+export default LoadingSpinner;
