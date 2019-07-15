a0ec6620b742c9964c57635c587c3813acd90da6
nick bouldien <nick.bouldien@gmail.com>
2019-07-14 22:59:44 -0500

    refactor to get appReducer working (though currently with a few any types in the actions - TODO)

--- src/actions/actionTypes.ts
+++ src/actions/actionTypes.ts
@@ -1,21 +1,21 @@
-export const ERROR: string = 'ERROR';
+export const ERROR = 'ERROR';
 
 /* admin */
-export const UPDATE_ITEM_AMT: string = 'UPDATE_ITEM_AMT';
+export const UPDATE_ITEM_AMT = 'UPDATE_ITEM_AMT';
 
 /* stock */
-export const FETCH_STOCK_ITEMS: string = 'FETCH_STOCK_ITEMS';
-export const FETCH_ITEMS_SUCCESS: string = 'FETCH_ITEMS_SUCCESS';
-export const ADD_NEW_ITEM_TO_STOCK: string = 'ADD_NEW_ITEM_TO_STOCK';
-export const REQUEST_ITEMS_IN_STOCK: string = 'REQUEST_ITEMS_IN_STOCK';
+export const FETCH_STOCK_ITEMS = 'FETCH_STOCK_ITEMS';
+export const FETCH_ITEMS_SUCCESS = 'FETCH_ITEMS_SUCCESS';
+export const ADD_NEW_ITEM_TO_STOCK = 'ADD_NEW_ITEM_TO_STOCK';
+export const REQUEST_ITEMS_IN_STOCK = 'REQUEST_ITEMS_IN_STOCK';
 
 /* cart */
-export const BUY_CART_ITEMS: string = 'BUY_CART_ITEMS';
-export const ADD_TO_CART: string = 'ADD_TO_CART';
-export const ADD_ITEM_TO_CART: string = 'ADD_ITEM_TO_CART';
-export const REMOVE_FROM_CART: string = 'REMOVE_FROM_CART';
-export const REMOVE_ITEM_FROM_CART: string = 'REMOVE_ITEM_FROM_CART';
-export const FETCH_CART_ITEMS: string = 'FETCH_CART_ITEMS';
+export const BUY_CART_ITEMS = 'BUY_CART_ITEMS';
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const ADD_ITEM_TO_CART = 'ADD_ITEM_TO_CART';
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const REMOVE_ITEM_FROM_CART = 'REMOVE_ITEM_FROM_CART';
+export const FETCH_CART_ITEMS = 'FETCH_CART_ITEMS';
 
 /* sagas */
-export const BUY_ITEMS: string = 'BUY_ITEMS';
+export const BUY_ITEMS = 'BUY_ITEMS';
