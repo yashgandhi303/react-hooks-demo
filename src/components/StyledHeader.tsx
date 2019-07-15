6c129a599dd3898b639f53c7fe52f58f4a90417c
nick bouldien <nick.bouldien@gmail.com>
2019-07-14 21:40:20 -0500

    fix StyledHeader HOC issue, migrate routes to a ts file

--- src/components/StyledHeader.tsx
+++ src/components/StyledHeader.tsx
@@ -1,46 +1,52 @@
-import React, { useContext, useState } from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import React, {useContext, useState} from 'react';
+import {Link, withRouter, RouteComponentProps} from 'react-router-dom';
+import * as H from 'history';
 import Toggle from 'react-toggle';
-import { Button, Icon, Menu } from 'semantic-ui-react';
-
-import { logout } from '../auth';
+import {Button, Icon, Menu} from 'semantic-ui-react';
+import {logout} from '../auth';
 import * as ROUTES from '../constants/routes';
-import { ThemeContext } from '../providers/ThemeProvider';
+import {ThemeContext} from '../providers/ThemeProvider';
 
-const StyledHeader = ({ authUser, history }) => {
+interface IProps extends RouteComponentProps {
+  // extends React.ComponentProps<any>
+  authUser: firebase.User | null;
+  history: H.History;
+}
+
+const StyledHeader: React.FC<IProps> = ({authUser, history}) => {
   const [activeItem, setActiveItem] = useState(window.location.pathname);
   const [theme, setTheme] = useContext(ThemeContext);
 
   const updateTheme = () => {
-    const newTheme = theme === "light" ? "dark" : "light";
+    const newTheme = theme === 'light' ? 'dark' : 'light';
     setTheme(newTheme);
   };
 
   const logoutUser = () => {
     logout()
-      .then( () => {
+      .then(() => {
         history.push(ROUTES.LOGIN);
       })
       .catch(err => {
-        console.error("err: ", err);
+        console.error('err: ', err);
         history.push(ROUTES.LOGIN);
-      })
+      });
   };
 
   return (
     <Menu stackable inverted pointing>
       <Menu.Item
-        name='home'
+        name="home"
         active={activeItem === ROUTES.HOME}
         onClick={() => setActiveItem(ROUTES.HOME)}
         as={Link}
         to={ROUTES.HOME}
       >
-        Carrinho.com <Icon name={'shopping cart'} size={"large"} />
+        Carrinho.com <Icon name={'shopping cart'} size={'large'} />
       </Menu.Item>
 
       <Menu.Item
-        name='about'
+        name="about"
         active={activeItem === ROUTES.ABOUT}
         onClick={() => setActiveItem(ROUTES.ABOUT)}
         as={Link}
@@ -50,7 +56,7 @@ const StyledHeader = ({ authUser, history }) => {
       </Menu.Item>
 
       <Menu.Item
-        name='admin'
+        name="admin"
         active={activeItem === ROUTES.ADMIN}
         onClick={() => setActiveItem(ROUTES.ADMIN)}
         as={Link}
@@ -61,45 +67,49 @@ const StyledHeader = ({ authUser, history }) => {
 
       <Menu.Item>
         <Toggle
-          aria-label={"toggle theme mode"}
-          className='theme-toggle'
-          defaultChecked={theme === "dark"}
+          aria-label={'toggle theme mode'}
+          className="theme-toggle"
+          defaultChecked={theme === 'dark'}
           icons={{
-            checked: <Icon name='moon' color={"yellow"} />,
-            unchecked: <Icon name='sun' color={"yellow"} />
+            checked: <Icon name="moon" color={'yellow'} />,
+            unchecked: <Icon name="sun" color={'yellow'} />,
           }}
           onChange={() => updateTheme()}
         />
       </Menu.Item>
 
-      <Menu.Menu position='right'>
+      <Menu.Menu position="right">
         <Menu.Item
-          name='cart'
+          name="cart"
           active={activeItem === ROUTES.CART}
           onClick={() => setActiveItem(ROUTES.CART)}
           as={Link}
           to={ROUTES.CART}
         >
-          Cart <Icon name={'cart'} size={"large"} color={"blue"} />
+          Cart <Icon name={'cart'} size={'large'} color={'blue'} />
         </Menu.Item>
 
-        { authUser ?
-          <Menu.Item className='item'>
+        {authUser ? (
+          <Menu.Item className="item">
             <Button onClick={logoutUser}>Logout</Button>
           </Menu.Item>
-          :
+        ) : (
           <>
             <Menu.Item>
-              <Button as={Link} to={ROUTES.REGISTER} inverted color='black'>Sign Up</Button>
+              <Button as={Link} to={ROUTES.REGISTER} inverted color="black">
+                Sign Up
+              </Button>
             </Menu.Item>
-            <Menu.Item className='item'>
-              <Button as={Link} to={ROUTES.LOGIN}>Log in</Button>
+            <Menu.Item className="item">
+              <Button as={Link} to={ROUTES.LOGIN}>
+                Log in
+              </Button>
             </Menu.Item>
           </>
-        }
+        )}
       </Menu.Menu>
     </Menu>
-  )
+  );
 };
 
 export default withRouter(StyledHeader);
