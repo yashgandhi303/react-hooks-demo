777953e9e880ae4283663442e5ea3f860407d0ba
Yash Gandhi <yashgandhi3037@gmail.com>
2020-03-16 08:51:35 +0530

    rename components

--- src/components/OnBoarding/OnBoarding.tsx
+++ src/components/OnBoarding/OnBoarding.tsx
@@ -3,9 +3,9 @@ import { Helmet } from 'react-helmet';
 import { ThemeContext } from '../../providers/ThemeProvider';
 import { useAppState } from '../../providers/AppProvider';
 import * as H from 'history';
-import Step1 from './Step1';
-import Step2 from './Step2';
-import Step3 from './Step3';
+import Step1 from './OnBoardingStep1';
+import Step2 from './OnBoardingStep2';
+import Step3 from './OnBoardingStep3';
 interface IProps {
     history: H.History;
 }
